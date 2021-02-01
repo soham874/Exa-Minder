782cf2603320fd223d4f2682a3e55cf5b094cce3
soham874 <soham874@gmail.com>
2021-02-01 20:16:14 +0530

    Refactored : Modified code

--- Exa-Minder/src/App.js
+++ Exa-Minder/src/App.js
@@ -4,7 +4,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className={"App"}>
       <Router>
         <Switch>
           <Route path="/" component={Login} />
