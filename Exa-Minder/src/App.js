3d128f57197daf3f224256509ba2b5610c2c3935
soham874 <soham874@gmail.com>
2021-02-02 18:54:42 +0530

    Refactored : Modified code

--- Exa-Minder/src/App.js
+++ Exa-Minder/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Login from './Pages/Login/login'
 import Registration from './Pages/Registration/registration'
@@ -9,15 +9,15 @@ import Reset from './Pages/Reset-Password/ResetPassword'
 function App() {
   return (
     <div className={"App"}>
-      <Router>
+      
         <Switch>
           <Redirect path="/" exact to="/login"/>
-          <Route path="/Exa-Minder/login" exact component={Login} />
+          <Route path="/login" exact component={Login} />
           <Route path="/registration" component={Registration} />
           <Route path="/forgot-password" component={Forgot} />
           <Route path="/resetpassword" component={Reset} />
         </Switch>
-      </Router>
+      
     </div>
   );
 }
